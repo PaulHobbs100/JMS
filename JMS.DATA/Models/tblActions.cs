e0e80d90136a1177bb32360ccd564657534c2e31
Paul Hobbs <paulhobbs100@hotmail.com>
2018-04-06 20:51:53 +0100

    Initial creation of jms.data project. compatible with legacy application, NO optimisations, non standard naming conventions

---
+++ JMS.DATA/Models/tblActions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JMS.DATA.Models
+{
+    /*************************************************************************
+     * Table: tblActions
+     * Notes: 
+     * Version: 0.1, Initial conversion of Access97 table
+     *************************************************************************/
+    class tblActions
+    {
+        public int fldActionID;
+        public int fldActionType;
+        public int fldActionStatus;
+        public string fldNotes;
+        public DateTime fldActionDate;
+        public string fldActionCreatedBy;
+        public string fldActionFor;
+        public int fldClientID;
+        public string fldClientContact;
+    }
+}
---
+++ JMS.DATA/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace JMS.DATA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main dummy method");
+            Console.ReadLine();
+        }
+    }
+}
