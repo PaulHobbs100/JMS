4909ddbecaa1cd7266d127474ab323cc0c398f3c
Paul Hobbs <paulhobbs100@hotmail.com>
2018-04-07 01:16:39 +0100

    Adding tables, legacy schema

---
+++ JMS.DATA/Models/tblClient_Contract.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JMS.DATA.Models
+{   /*************************************************************************
+     * Table: tblClient_Contract
+     * Notes: 
+     * Version: 0.1, Initial conversion of Access97 table
+     *************************************************************************/
+    class tblClient_Contract
+    {
+        public int fldClientID;
+        public int fldContactGroupID;
+        public string fldTitle;
+        public string fldContact;
+        public string fldContactLastName;
+        public int fldContactID;
+        public string fldPhone;
+        public string fldPosition;
+        public DateTime fldLastUpdate;
+        public bool fldDeleted;
+        public string fldAddress1;
+        public string fldAddress2;
+        public string fldTown;
+        public string fldCounty;
+        public string fldPostcode;
+        public string fldCountry;
+        public bool fldArchived;
+        public DateTime fldDateArchived;
+        public string fldMobile;
+        public string fldEmail;
+        public string fldFAX;
+        public bool fldSendTimesheet;
+    }
+}
---
+++ JMS.DATA/Models/tblClient_Rate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JMS.DATA.Models
+{   /*************************************************************************
+     * Table: tblClient_Rate
+     * Notes: 
+     * Version: 0.1, Initial conversion of Access97 table
+     *************************************************************************/
+    class tblClient_Rate
+    {
+        public int fldID;
+        public int fldClientID;
+        public int fldCostCentreID;
+        public decimal fldBasicCharge;
+        public string fldBasicDayHours;
+        public decimal fldBasicPay;
+        public string fldName;
+        public decimal fldNightAllowance;
+        public string fldNote;
+        public decimal fldOvertimeCharge;
+        public string fldOvertimeHours;
+        public decimal fldOvertimePay;
+        public int fldPartOfDay;
+        public decimal fldSundayCharge;
+        public string fldSundayHours;
+        public decimal fldSundayPay;
+        public string fldWorkingHours;
+        public DateTime fldLastUpdate;
+        public bool fldInactive;
+        public bool fldArchived;
+        public DateTime fldDateArcived;
+        public decimal fldTransportCharge;
+        public bool fldOverideDefaultTransportCharge;
+        public decimal fldOvr2Chg;
+        public string fldOvr2Hrs;
+        public decimal fldOvr2Pay;
+        public string fldNote2;
+        public string fldNote3;
+
+
+    }
+}
