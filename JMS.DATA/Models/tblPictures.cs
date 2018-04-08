c2affb56260af8f51c0b41e414ab550e813f6b3b
Paul Hobbs <paulhobbs100@hotmail.com>
2018-04-08 02:28:19 +0100

    table updates

---
+++ JMS.DATA/Models/tblPictures.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JMS.DATA.Models
+{/*************************************************************************
+     * Table: tblPictures
+     * Notes: 
+     * Version: 0.1, Initial conversion of Access97 table
+     *************************************************************************/
+
+    class tblPictures
+    {
+        public int fldPictureTypeID;
+        public byte[] fldPicture;
+        public string fldDescription;
+    }
+}
---
+++ JMS.DATA/Models/tblRateAttributeChageLog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JMS.DATA.Models
+{/*************************************************************************
+     * Table: tblRateAttributeChangeLog
+     * Notes: 
+     * Version: 0.1, Initial conversion of Access97 table
+     *************************************************************************/
+
+    class tblRateAttributeChageLog
+    {
+    public int fldLogID;
+    public int fldRateID;
+    public int fldAttributeID;
+    public DateTime fldChangeDate;
+    public string fldName;
+    }
+}
